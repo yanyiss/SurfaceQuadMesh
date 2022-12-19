4692e563fbb7cf78254a38bdd9b9c3dfd5a49a90
yanyiss <61866455+yanyiss@users.noreply.github.com>
2022-12-19 17:40:41 +0800

    update

--- src/Algorithm/covering_space.h
+++ src/Algorithm/covering_space.h
@@ -7,20 +7,54 @@ namespace LoopGen
 	class CoveringSpaceDataStructure
 	{
 	public:
-		CoveringSpaceDataStructure(Mesh* mesh_) : base_mesh(mesh_) { }
+		CoveringSpaceDataStructure(Mesh* mesh_) : mesh(mesh_) { }
 		~CoveringSpaceDataStructure() {}
 
 	public:
-		Mesh* base_mesh;
+		Mesh* mesh;
 		crossField* cf;
 
+		std::deque<bool> sing_flag;
 		std::vector<std::vector<int>> vv;
 		std::vector<std::vector<int>> fv;
 
 		void set_cf(crossField* cf_) { cf = cf_; }
 		void update()
 		{
+			int nv = mesh->n_vertices();
+			int ne = mesh->n_edges();
 
+			sing_flag.resize(nv, false);
+			for (auto sing : cf->getSingularity())
+				sing_flag[sing] = true;
+
+			std::vector<int> h_shift(ne << 1);
+			auto& matching = cf->getMatching();
+			for (auto th = mesh->halfedges_begin(); th != mesh->halfedges_end(); ++th)
+			{
+				auto h = th.handle();
+				auto fromv = mesh->from_vertex_handle(h);
+				auto tov = mesh->to_vertex_handle(h);
+				int fromid = fromv.idx();
+				int toid = tov.idx();
+				if ((sing_flag[fromid] || sing_flag[toid]) && fromid < toid)
+					continue;
+
+				int index = 0;
+				auto h0 = mesh->voh_begin(fromv).handle();
+				do
+				{
+					h0 = mesh->opposite_halfedge_handle(mesh->prev_halfedge_handle(h0));
+					index += matching[h0.idx()]; index %= 4;
+				} while (h0 != h);
+				auto h1 = mesh->voh_begin(tov).handle();
+				do
+				{
+					h1 = mesh->opposite_halfedge_handle(mesh->prev_halfedge_handle(h1));
+					index += 4 - matching[h1.idx()]; index %= 4;
+				} while (h1 != h);
+
+			}
 		}
 	};
 	typedef CoveringSpaceDataStructure<2> M2;
